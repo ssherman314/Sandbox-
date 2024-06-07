051cd5f1f657b44efd984a08de75b98bc544051b
CrimsonMartin <maf152@case.edu>
2024-06-07 14:09:04 -0500

    switching to lazy loading with polars, the csvs are huge

--- ParseTPM.py
+++ ParseTPM.py
@@ -1,19 +1,31 @@
-import pandas
+import pandas as pd
+import polars as pl
 
 
-MedianTPM = pandas.read_csv('venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct', sep='\t', skiprows=2)
-# GeneReads = pandas.read_csv('venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct', sep = '\t', skiprows=2)
-# GeneTPM = pandas.read_csv('venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct', sep = '\t', skiprows=2)
+# import time
+# t0 = time.time()
 
-# TranscriptExpectedCount = pandas.read_csv('venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.gct', sep = '\t', skiprows=2)
-# TranscriptTPM = pandas.read_csv('venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.gct', sep = '\t', skiprows=2)
+# # stuff here
 
-# Junctions = pandas.read_csv('venv/GTEx_Analysis_2017-06-05_v8_STARv2.5.3a_junctions.gct', sep = '\t', skiprows=2)
-# ExonReads = pandas.read_parquet('venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_exon_reads.parquet')
+# print("it took (seconds): ", time.time()-t0)
 
 
 
-print(MedianTPM.head)
+MedianTPM = pl.scan_csv('venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.csv', separator='\t')
+GeneReads = pl.scan_csv('venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.csv', separator='\t')
+GeneTPM = pl.scan_csv('venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.csv', separator='\t')
+
+TranscriptExpectedCount = pl.scan_csv('venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.csv', separator='\t')
+TranscriptTPM = pl.scan_csv('venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.csv', separator='\t')
+
+Junctions = pl.scan_csv('venv/GTEx_Analysis_2017-06-05_v8_STARv2.5.3a_junctions.csv', separator='\t')
+
+ExonReads = pl.scan_parquet('venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_exon_reads.parquet')
+
+
+print(MedianTPM.fetch(5))
+# print(ExonReads.fetch(5))
+
 
 #do stuff here - what are we looking for in this?
 
--- setup.sh
+++ setup.sh
@@ -2,39 +2,51 @@
 #   Download the GTEx v8 data (if it's not already downloaded)
 
 #	Gene read counts.
-if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct ]; then
+if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.csv ]; then
     wget -nc -P venv/ "https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz"
     gunzip venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz
+    sed '1,2d' venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct > venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.csv
+    rm venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct
 fi
 
 #	Gene TPMs.
-if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct ]; then
+if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.csv ]; then
     wget -nc -P venv/ "https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz"
     gunzip venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz
+    sed '1,2d' venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct > venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.csv
+    rm venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct
 fi
 
 #   Median gene-level TPM by tissue. Median expression was calculated from the file GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz.
-if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct ]; then
+if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.csv ]; then
     wget -nc -P venv/ "https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct.gz"
     gunzip venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct.gz
+    sed '1,2d' venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct > venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.csv
+    rm venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct
 fi
 
 #   Exon-exon junction read counts.
-if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_STARv2.5.3a_junctions.gct ]; then
+if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_STARv2.5.3a_junctions.csv ]; then
     wget -nc -P venv/ "https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_STARv2.5.3a_junctions.gct.gz"
     gunzip venv/GTEx_Analysis_2017-06-05_v8_STARv2.5.3a_junctions.gct.gz
+    sed '1,2d' venv/GTEx_Analysis_2017-06-05_v8_STARv2.5.3a_junctions.gct > venv/GTEx_Analysis_2017-06-05_v8_STARv2.5.3a_junctions.csv
+    rm venv/GTEx_Analysis_2017-06-05_v8_STARv2.5.3a_junctions.gct
 fi
 
 # Transcript read counts.
-if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct ]; then
+if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.csv ]; then
     wget -nc -P venv/ "https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.gct.gz"
-    gunzip venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz
+    gunzip venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.gct.gz
+    sed '1,2d' venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.gct > venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.csv
+    rm venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.gct
 fi
 
 #	Transcript TPMs.
-if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct ]; then
+if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.csv ]; then
     wget -nc -P venv/ "https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.gct.gz"
-    gunzip venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz
+    gunzip venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.gct.gz
+    sed '1,2d' venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.gct > venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.csv
+    rm venv/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.gct
 fi
 
 #   Exon read counts.
@@ -42,6 +54,10 @@ if [ ! -f venv/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_exon_reads.parquet ]; t
     wget -nc -P venv/ "https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_exon_reads.parquet"
 fi
 
+
+
+
+#   These are the subject phenotypes for the GTEx v8 data.
 
 
 #   A data dictionary that describes each column in the GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt.
